9f73afe2ebfe457d0a67fd336056d58080cc034b
saiful201 <118247523+saiful201@users.noreply.github.com>
2025-03-04 10:31:11 +0300

    Update and rename daru.js to মদ.js

--- Saiful/commands/মদ.js
+++ Saiful/commands/মদ.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "daru",
+	name: "মদ",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "Priyansh", 
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Daru")==0 || event.body.indexOf("daru")==0 || event.body.indexOf("Drink")==0 || event.body.indexOf("drink")==0) {
 		var msg = {
-				body: "Aajao milke pite hai 🍻🍷🍺",
+				body: "আসো সবাই মিলে মিশে মদ খাই 🍻🍷🍺",
 				attachment: fs.createReadStream(__dirname + `/noprefix/daru.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
