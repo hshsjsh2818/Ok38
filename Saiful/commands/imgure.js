59584f6284d77b21e0911b175eddc52ed3203575
saiful201 <118247523+saiful201@users.noreply.github.com>
2025-03-11 11:23:45 +0300

    Update imgure.js

--- Saiful/commands/imgure.js
+++ Saiful/commands/imgure.js
@@ -2,21 +2,21 @@ module.exports.config = {
     name: "imgur",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "",
-    commandCategory: "Game",
-    usages: "[reply]",
+    credits: "Joshua Sy",
+    description: "imgur upload",
+    commandCategory: "imgur",
+    usages: "[reply to image]",
     cooldowns: 5,
     dependencies: {
-      "axios": ""
-    }
+  "axios": "",}
 };
-
+ 
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if(!linkanh) return api.sendMessage('Please reply or enter a link 1 image!!!', event.threadID, event.messageID)
-const res = await axios.get(`https://imgur-api-by-koja.xx0xkoja.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
-var img = res.data.uploaded.image;
-    return api.sendMessage(`${img}`, event.threadID, event.messageID);
+    if(!linkanh) return api.sendMessage('Please reply to image.', event.threadID, event.messageID)
+const res = await axios.get(`https://api.phamvandien.xyz/imgur?link=${encodeURIComponent(linkanh)}`);    
+var juswa = res.data.uploaded.image;
+    return api.sendMessage(`Here is your imgur link:\n\n${juswa}`, event.threadID, event.messageID);
+ 
 }
